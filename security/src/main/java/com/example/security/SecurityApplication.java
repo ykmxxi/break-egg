cec3cbcdd90e23ab7fc249618d4cfb0300fade3f
ykmxxi <ykmxxi97@gmail.com>
2024-04-15 12:29:43 +0900

    feat: view 구현
    - thymeleaf + spring security 사용

--- security/src/main/java/com/example/security/SecurityApplication.java
+++ security/src/main/java/com/example/security/SecurityApplication.java
@@ -2,12 +2,15 @@ package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+// localhost:8080 접속시 바로 기본 시큐리티가 제공하는 로그인 페이지로 이동하기 때문에 제외
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
 
 }
