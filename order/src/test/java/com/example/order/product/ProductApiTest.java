cfcf39caf4dfabacecee8b12942136d5c0bdf91a
ykmxxi <ykmxxi97@gmail.com>
2024-04-19 11:32:10 +0900

    #1 refactor: 중복되는 요청 클래스로 분리

--- order/src/test/java/com/example/order/product/ProductApiTest.java
+++ order/src/test/java/com/example/order/product/ProductApiTest.java
@@ -1,15 +1,12 @@
 package com.example.order.product;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import com.example.order.ApiTest;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 
@@ -17,49 +14,26 @@ class ProductApiTest extends ApiTest {
 
     @Test
     void 상품등록_API() {
-        AddProductRequest request = createRequest();
+        AddProductRequest request = ProductSteps.상품등록요청_생성();
 
         // API 요청
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/products")
-                .then()
-                .log().all().extract();
+        ExtractableResponse<Response> response = ProductSteps.상품등록요청(request);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @Test
     void 상품조회_API() {
-        AddProductRequest request = createRequest();
+        AddProductRequest request = ProductSteps.상품등록요청_생성();
 
-        ExtractableResponse<Response> productAddResponse = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/products")
-                .then()
-                .log().all().extract();
+        ExtractableResponse<Response> productAddResponse = ProductSteps.상품등록요청(request);
 
         Long productId = 1L;
 
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .get("/products/{productId}", productId)
-                .then().log().all()
-                .extract();
+        ExtractableResponse<Response> response = ProductSteps.상품조회요청(productId);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
-    }
-
-    private static AddProductRequest createRequest() {
-        String name = "상품명";
-        int price = 1000;
-        DiscountPolicy discountPolicy = DiscountPolicy.NONE;
-        return new AddProductRequest(name, price, discountPolicy);
     }
 
 }
---
+++ order/src/test/java/com/example/order/product/ProductSteps.java
@@ -0,0 +1,36 @@
+package com.example.order.product;
+
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class ProductSteps {
+
+    public static ExtractableResponse<Response> 상품등록요청(final AddProductRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+    }
+
+    public static AddProductRequest 상품등록요청_생성() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        return new AddProductRequest(name, price, discountPolicy);
+    }
+
+    public static ExtractableResponse<Response> 상품조회요청(final Long productId) {
+        return RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+    }
+
+}
