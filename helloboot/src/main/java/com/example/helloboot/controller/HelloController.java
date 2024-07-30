992075c5180a38b3a327944d5ccb93da23e182dc
ykmxxi <ykmxxi97@gmail.com>
2024-07-30 18:32:08 +0900

    feat: HelloController 구현과 테스트

---
+++ helloboot/src/main/java/com/example/helloboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.helloboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") final String name) {
+        return "Hello " + name;
+    }
+
+}
