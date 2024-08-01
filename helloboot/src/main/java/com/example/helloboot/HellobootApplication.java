10c0263d733ce0d7ae98ca550a3d91cf094f4fa3
ykmxxi <ykmxxi97@gmail.com>
2024-08-01 17:16:20 +0900

    feat: Servlet Container에 Servlet 등록

--- helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
@@ -1,14 +1,32 @@
 package com.example.helloboot;
 
+import java.io.IOException;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.MediaType;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext ->
+                servletContext.addServlet("hello", new HttpServlet() {
+                    @Override
+                    protected void service(final HttpServletRequest req, final HttpServletResponse resp)
+                            throws ServletException, IOException {
+                        // resp.setStatus(HttpStatus.OK.value()); // OK 상태 코드는 생략 가능
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello Servlet");
+                    }
+                }).addMapping("/hello")
+        );
         webServer.start();
     }
 
