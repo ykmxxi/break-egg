9c1b58b9743b48797ec71afc8346e5e210fd262a
ykmxxi <ykmxxi97@gmail.com>
2024-08-02 15:07:50 +0900

    feat: Front Controller 패턴 도입

--- helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import com.example.helloboot.controller.HelloController;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -16,17 +20,28 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext ->
-                servletContext.addServlet("hello", new HttpServlet() {
-                    @Override
-                    protected void service(final HttpServletRequest req, final HttpServletResponse resp)
-                            throws ServletException, IOException {
-                        // resp.setStatus(HttpStatus.OK.value()); // OK 상태 코드는 생략 가능
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(final HttpServletRequest req, final HttpServletResponse resp)
+                        throws ServletException, IOException {
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+                        String ret = helloController.hello(name);
+
+                        resp.setStatus(HttpStatus.OK.value());
                         resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello Servlet");
+                        resp.getWriter().println(ret);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
-                }).addMapping("/hello")
-        );
+                }
+            }).addMapping("/*");
+        });
         webServer.start();
     }
 
