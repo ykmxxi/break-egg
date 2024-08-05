5469878ad40e6a64a28394b2cb12c24f3f6a2cc7
ykmxxi <ykmxxi97@gmail.com>
2024-08-05 16:28:14 +0900

    refactor: 서블릿 컨테이너 초기화 작업을 스프링 컨테이너 초기화 시점에서 진행(spring boot의 방식)

--- helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
@@ -13,18 +13,24 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
         // 웹 컨테이너로 변경
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                // 서블릿 컨테이너를 초기화하고 띄우는 작업을 스프링 컨테이너 초기화 과정에서 진행
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this) // this,메서드가 속한 컨테이너 자체를 전달
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext) // DispatcherServlet 사용
-            ).addMapping("/*");
-        });
-        webServer.start();
     }
 
 }
