b12a0bd062fec30c0710b41799f20846d34c9494
ykmxxi <ykmxxi97@gmail.com>
2024-08-06 16:49:09 +0900

    test: hello API

---
+++ helloboot/src/test/java/com/example/helloboot/controller/HelloApiTest.java
@@ -0,0 +1,29 @@
+package com.example.helloboot.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+class HelloApiTest {
+
+    @Test
+    @DisplayName("TestRestTemplate 사용한 HTTP 응답 테스트")
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> response =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE))
+                .startsWith(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(response.getBody()).isEqualTo("Hello Spring");
+    }
+
+}
